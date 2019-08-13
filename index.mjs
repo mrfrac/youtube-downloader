77385ec68babb6e7f2949352a66561d4be56904b
mrfrac <fracturer@gmail.com>
2019-08-13 14:05:18 +0500

    fix styling

--- index.mjs
+++ index.mjs
@@ -5,49 +5,49 @@ import { existsSync, createReadStream } from "fs";
 import yargs from "yargs";
 
 const argv = yargs
-    .usage("$0 --input <file.txt> --provider <youtube|vimeo>")
-    .option("input", {
-        alias: "i",
-        describe: "input file with line-by-line links",
-        demand: true,
-        string: true,
-        coerce: (input) => {
-            if (!existsSync(input)) {
-                throw new Error(`Input file "${input}" not found!`.red);
-            }
-            return input;
-        }
-    })
-    .option("provider", {
-        describe: "Video provider (youtube or vimeo)",
-        demand: true,
-        string: true,
-        alias: "p",
-        coerce: (param) => {
-            const val = String(param).toLowerCase();
-            if (!["youtube", "vimeo"].includes(val)) {
-                throw new Error("Wrong provider name".red)
-            }
-            return val;
-        }
-    })
-    .option("out", {
-        describe: "Output directory",
-        string: true,
-        alias: "o",
-        coerce: (path) => {
-            if (!existsSync(path)) {
-                throw new Error(`Output directory "${path}" doesn't exists`.red)
-            }
-            return path;
-        }
-    })
-    .argv
+  .usage("$0 --input <file.txt> --provider <youtube|vimeo>")
+  .option("input", {
+    alias: "i",
+    describe: "input file with line-by-line links",
+    demand: true,
+    string: true,
+    coerce: (input) => {
+      if (!existsSync(input)) {
+        throw new Error(`Input file "${input}" not found!`.red);
+      }
+      return input;
+    }
+  })
+  .option("provider", {
+    describe: "Video provider (youtube or vimeo)",
+    demand: true,
+    string: true,
+    alias: "p",
+    coerce: (param) => {
+      const val = String(param).toLowerCase();
+      if (!["youtube", "vimeo"].includes(val)) {
+        throw new Error("Wrong provider name".red)
+      }
+      return val;
+    }
+  })
+  .option("out", {
+    describe: "Output directory",
+    string: true,
+    alias: "o",
+    coerce: (path) => {
+      if (!existsSync(path)) {
+        throw new Error(`Output directory "${path}" doesn't exists`.red)
+      }
+      return path;
+    }
+  })
+  .argv
 
 const readInterface = readline.createInterface({
-    input: createReadStream(argv.input),
-    output: null,
-    console: false
+  input: createReadStream(argv.input),
+  output: null,
+  console: false
 });
 
 const outputDirectory = argv.out || "./";
@@ -56,9 +56,9 @@ const provider = new providerClass();
 
 readInterface.on("line", (url) => {
 
-    provider.download(url, outputDirectory).then(() => {
-        console.log(`Done ${url}`);
-    }, (e) => {
-        console.log(`Error ${url}: ${e}`);
-    });
+  provider.download(url, outputDirectory).then(() => {
+    console.log(`Done ${url}`);
+  }, (e) => {
+    console.log(`Error ${url}: ${e}`);
+  });
 });
--- providers/youtube.mjs
+++ providers/youtube.mjs
@@ -3,38 +3,33 @@ import { existsSync, createWriteStream } from "fs";
 import { resolve } from "path"
 
 export default class YouTube {
-    constructor() {
-        this.providerName = "youtube";
-        this.params = {
+  static get providerName() {
+    return "youtube";
+  }
+
+  download(url, path = "./") {
+    return ytdl.getInfo(url).then((data) => {
+      let title = String(data.player_response.videoDetails.title).replace(/\r?\n|\r/g, '');
+      if (!title) {
+        title = `${Date.now()}`;
+      }
+      let fullpath = resolve(path, `${title}.mp4`);
+
+      if (existsSync(fullpath)) {
+        throw new Error(`File "${fullpath}" already exists`);
+      }
+
+      return new Promise((resolve, reject) => {
+        const writeStream = createWriteStream(fullpath);
+        ytdl(url, {
             filter: (format) => format.container === 'mp4'
-        };
-    }
-
-    static get providerName() {
-        return "youtube";
-    }
-
-    download(url, path = "./") {
-        return ytdl.getInfo(url).then((data) => {
-            let title = String(data.player_response.videoDetails.title).replace(/\r?\n|\r/g, '');
-            if (!title) {
-                title = `${Date.now()}`;
-            }
-            let fullpath = resolve(path, `${title}.mp4`);
-
-            if (existsSync(fullpath)) {
-                throw new Error(`File "${fullpath}" already exists`);
-            }
-
-            return new Promise((resolve, reject) => {
-                const writeStream = createWriteStream(fullpath);
-                ytdl(url, { filter: (format) => format.container === 'mp4' })
-                    .pipe(writeStream);
-                writeStream.on("finish", resolve);
-                writeStream.on("error", (error) => {
-                    reject(error);
-                });
-            });
+          })
+          .pipe(writeStream);
+        writeStream.on("finish", resolve);
+        writeStream.on("error", (error) => {
+          reject(error);
         });
-    }
+      });
+    });
+  }
 }
